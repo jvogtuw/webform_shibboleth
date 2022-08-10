65c10b2c6666d2011cf482f38dc4079b1f1c0ac1
Jeanna Vogt <jvogt@uw.edu>
2022-08-10 15:51:39 -0700

    Failed to grab the latest changes on the initial commit.

--- src/EventSubscriber/WebformShibbolethAccessSubscriber.php
+++ src/EventSubscriber/WebformShibbolethAccessSubscriber.php
@@ -17,48 +17,25 @@ use Symfony\Component\Routing\Route;
 class WebformShibbolethAccessSubscriber implements EventSubscriberInterface {
 
   /**
-   * @var \Drupal\Core\Config\ConfigFactoryInterface
-   */
-  private $configFactory;
-
-  /**
    * @var \Drupal\shibboleth\Authentication\ShibbolethAuthManager
    */
   private $shibAuthManager;
 
   /**
-   * Constructs a WebformExceptionHtmlSubscriber object.
+   * Constructs event subscriber.
    *
-   * @param \Symfony\Component\HttpKernel\HttpKernelInterface $http_kernel
-   *   The HTTP kernel.
-   * @param \Psr\Log\LoggerInterface $logger
-   *   The logger service.
-   * @param \Drupal\Core\Routing\RedirectDestinationInterface $redirect_destination
-   *   The redirect destination service.
-   * @param \Symfony\Component\Routing\Matcher\UrlMatcherInterface $access_unaware_router
-   *   A router implementation which does not check access.
-   * @param \Drupal\Core\Session\AccountInterface $account
-   *   The current user.
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-   *   The configuration object factory.
-   * @param \Drupal\Core\Render\RendererInterface $renderer
-   *   The renderer.
-   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
-   *   The messenger.
-   * @param \Drupal\webform\WebformTokenManagerInterface $token_manager
-   *   The webform token manager.
+   * @param \Drupal\shibboleth\Authentication\ShibbolethAuthManager $shib_auth
+   *   The Shibboleth authentication manager.
    */
-  public function __construct(/*HttpKernelInterface $http_kernel, LoggerInterface $logger, RedirectDestinationInterface $redirect_destination, UrlMatcherInterface $access_unaware_router, AccountInterface $account, */ConfigFactoryInterface $config_factory, ShibbolethAuthManager $shib_auth) {
-    // parent::__construct($http_kernel, $logger, $redirect_destination, $access_unaware_router);
-
-    // $this->account = $account;
-    $this->configFactory = $config_factory;
+  public function __construct(ShibbolethAuthManager $shib_auth) {
     $this->shibAuthManager = $shib_auth;
-    // $this->renderer = $renderer;
-    // $this->messenger = $messenger;
-    // $this->tokenManager = $token_manager;
   }
 
+  /**
+   * Checks if a Webform requires
+   * @param \Symfony\Component\HttpKernel\Event\RequestEvent $event
+   *   The request event.
+   */
   public function onRequestCheckWebformAccess(RequestEvent $event) {
     $url = Url::fromUserInput($event->getRequest()->getPathInfo());
     if (!$url) {
@@ -72,10 +49,8 @@ class WebformShibbolethAccessSubscriber implements EventSubscriberInterface {
 
     $webform = Webform::load($route_parameters['webform']);
     $access_rules = $webform->getAccessRules();
-    dpm($access_rules, 'access rules');
     $shibboleth_required = $access_rules['create']['shibboleth'];
     if ($shibboleth_required) {
-      \Drupal::messenger()->addStatus('shibboleth session is required');
       if (!$this->shibAuthManager->sessionExists()) {
         $auth_redirect = $this->shibAuthManager->getAuthenticateUrl();
         $response = new TrustedRedirectResponse($auth_redirect->toString());
@@ -83,10 +58,8 @@ class WebformShibbolethAccessSubscriber implements EventSubscriberInterface {
         return;
       }
     }
-    // \Drupal::messenger()->addStatus('webform access check event subscriber');
-    // dpm($event, 'webform_shibboleth request event');
-    dpm($route_parameters, 'webform_shibboleth route_parameters');
   }
+
   /**
    * {@inheritdoc}
    */
@@ -94,8 +67,6 @@ class WebformShibbolethAccessSubscriber implements EventSubscriberInterface {
     return [
       // Perform before ShibbolethPath access check.
       KernelEvents::REQUEST => ['onRequestCheckWebformAccess', 36],
-      // Perform before AuthenticationSubscriber->onExceptionAccessDenied()
-      // KernelEvents::EXCEPTION => ['onShibbolethSessionException', 70],
     ];
   }
 
